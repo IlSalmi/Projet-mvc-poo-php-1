7e35f8863764700ca7ec406f2a39163528d4b3ac
ilSalmi <ilias.salmi1808@gmail.com>
2022-12-07 11:54:15 +0100

    Front -mise en place du tableau liste jeux

--- games.php
+++ games.php
@@ -1,10 +1,47 @@
 <?php ob_start(); ?>
 
-<p>Games - Notre selection</p>
+
+<table class="table table-hover text-center shadow">
+  <thead>
+    <tr>
+      <th scope="col">Titre</th>
+      <th scope="col">Nombre de Joueur</th>
+      <th colspan="2">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table">
+      <td>Among us</th>
+      <td>15</td>
+      <td><i class="fa-solid fa-pen-to-square"></i></td>
+      <td><i class="fa-solid fa-trash text-danger"></i></td>
+
+    </tr>
+    <tr class="table">
+      <td>La League des legendes</th>
+      <td>10</td>
+      <td><i class="fa-solid fa-pen-to-square"></i></td>
+      <td><i class="fa-solid fa-trash text-danger"></i></td>
+
+    </tr>
+    <tr class="table">
+      <td>Starcraft</th>
+      <td>8</td>
+      <td><i class="fa-solid fa-pen-to-square"></i></td>
+      <td><i class="fa-solid fa-trash text-danger"></i></td>
+
+    </tr>
+  </tbody>
+</table>
+
+<a href="" class="btn btn-success d-block m-auto w-25">Ad a game <br>
+    <i class="fa-solid fa-gamepad"></i>
+</a>
 
 <?php
 $content = ob_get_clean();
 $title="Liste de jeux";
 require_once "base.html.php";
+
 
 ?>
