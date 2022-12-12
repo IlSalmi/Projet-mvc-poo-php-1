ec11c1c3e66d822e0f6ae1d28326e14c3f8aef0b
Amar2050 <amarboutayeb@gmail.com>
2022-12-12 16:27:00 +0100

    Début du ROOTING simple - affichage de vue

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-require_once "view/home.view.php";
+var_dump($_GET);
+if (empty($_GET['page'])) {
+    require_once "view/home.view.php";
+}else {
+    switch ($_GET['page']) {
+        case 'accueil': require_once "view/home.view.php";
+        break;
+    }
+}
